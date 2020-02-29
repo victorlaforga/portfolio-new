cf534dd8aebd4c178a785e970165719ca28424a8
Victor <victorlaforga@hotmail.com>
2020-02-29 04:41:47 +0100

    finished :D

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// $(document).ready(function() {
-//   //responsive menu toggle
-//   $("#menutoggle").click(function() {
-//     $('.xs-menu').toggleClass('displaynone');
-
-//     });
-//   //add active class on menu
-//   $('ul li').click(function(e) {
-//     e.preventDefault();
-//     $('li').removeClass('active');
-//     $(this).addClass('active');
-//   });
-// //drop down menu	
-//     $(".drop-down").hover(function() {
-//       $('.mega-menu').addClass('display-on');
-//     });
-//     $(".drop-down").mouseleave(function() {
-//       $('.mega-menu').removeClass('display-on');
-//     });
-
-// });
 
 
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -34,7 +13,12 @@ function myFunction() {
   const span = document.getElementsByClassName("close")[0];
   
   function openModal() {
-    modal.style.display = "block";
+    if(window.innerWidth > 850) {
+      modal.style.display = "block";
+    } else {
+      window.open("https://github.com/victorlaforga/Dating-app-pt")
+      
+    }
   }
   btn.addEventListener("click", openModal);
   function displayModal() {
@@ -53,7 +37,12 @@ function myFunction() {
   const span2 = document.getElementsByClassName("close")[1];
   
   function openModal2() {
-    modal2.style.display = "block";
+    if(window.innerWidth > 850) {
+      modal2.style.display = "block";
+    } else {
+      window.open("https://github.com/victorlaforga/nibud-budgethuis")
+      
+    }
   }
   btn2.addEventListener("click", openModal2);
   function displayModal2() {
@@ -68,13 +57,22 @@ function myFunction() {
   window.addEventListener("click", closeModal2);
 
 
+function goToGithubKids(){
+  window.open("https://github.com/victorlaforga/frontend-data");
+}
+document.getElementById("kidstreasures").addEventListener("click", goToGithubKids);
+
+function goToGithubDiner(){
+  window.open("https://github.com/victorlaforga/project-tech");
+}
+document.getElementById("dinerfortwo").addEventListener("click", goToGithubDiner);
+
 const $element=$('.each-event, .title');
 const $window = $(window);
 $window.on('scroll resize', check_for_fade);
 $window.trigger('scroll');
 function check_for_fade() { 
     const window_height = $window.height();
-    
     $.each($element, function (event) {
         const $element = $(this);
         const element_height = $element.outerHeight();
@@ -82,10 +80,53 @@ function check_for_fade() {
         space = window_height - (element_height + element_offset -$(window).scrollTop());
         if (space < 60) {
             $element.addClass("non-focus");
+            
         } else {
             $element.removeClass("non-focus");
         }
  
     });
 };
+
+
+function openKids() {
+  window.open("https://github.com/victorlaforga/frontend-data");
+}
+function goToPageOne() {
+  if(window.innerHeight < 750) {
+    window.scrollTo(0,660);
+  } else {
+    window.scrollTo(0,689);
+  }
+  
+  document.getElementById("myTopnav").classList.remove("responsive");
+  console.log("test");
+}
+
+document.getElementById("me").addEventListener("click", goToPageOne);
+
+function goToPagetwo() {
+  if(window.innerHeight < 750) {
+    window.scrollTo(0,2280);
+  } else {
+    window.scrollTo(0,1370);
+  }
+    document.getElementById("myTopnav").classList.remove("responsive");
+    
+    
+  }
+  document.getElementById("timeline").addEventListener("click", goToPagetwo);
+
+function goToPageWork() {
+  if(window.innerHeight < 750) {
+    window.scrollTo(0,3520);
+  } else {
+    window.scrollTo(0,2685);
+  }
+  
+  document.getElementById("myTopnav").classList.remove("responsive");
+  
+}
+
+document.getElementById("work").addEventListener("click", goToPageWork);
 
